44f9e7a15cfdd1862cfa9283fcd3fe86dbcbfa37
Quentin ANIERE <quentin.aniere@gmail.com>
2023-02-15 17:18:04 -0500

    Update home page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-import { BannerComponent } from './banner/banner.component';
-import { FooterComponent } from './footer/footer.component';
-import { NavigationComponent } from './navigation/navigation.component';
+import { HomeComponent } from './home/home.component';
 
 const routes: Routes = [
-  {path:'',component:BannerComponent},
+  {path:'',component:HomeComponent},
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+
 import { NavigationComponent } from './navigation/navigation.component';
-import { BannerComponent } from './banner/banner.component';
-import { FooterComponent } from './footer/footer.component';
+import { HomeComponent } from './home/home.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     NavigationComponent,
-    BannerComponent,
-    FooterComponent,
+    HomeComponent,
   ],
   imports: [
     BrowserModule,
