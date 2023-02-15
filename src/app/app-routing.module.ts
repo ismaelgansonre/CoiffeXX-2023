cd5acde3df3c8c1079fec230513fdf767c39e3fb
Quentin ANIERE <quentin.aniere@gmail.com>
2023-02-15 16:24:58 -0500

    Fix routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,14 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+
 import { BannerComponent } from './banner/banner.component';
 import { FooterComponent } from './footer/footer.component';
 import { NavigationComponent } from './navigation/navigation.component';
 
-
 const routes: Routes = [
-  {path:'footer',component:FooterComponent},
-  {path:'banner',component:BannerComponent},
-  {path:'navigation',component:NavigationComponent},
+  {path:'',component:BannerComponent},
 ];
 
 @NgModule({
